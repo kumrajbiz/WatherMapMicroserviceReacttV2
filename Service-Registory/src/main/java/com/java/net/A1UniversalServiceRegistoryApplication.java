eccc8ac3d79e5c102a29ebabf4538f61ab0d9440
kumrajbiz <126977951+kumrajbiz@users.noreply.github.com>
2023-05-10 14:03:31 +0530

    Service registry V2
    Include docker file

---
+++ Service-Registory/src/main/java/com/java/net/A1UniversalServiceRegistoryApplication.java
@@ -0,0 +1,19 @@
+package com.java.net;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/*
+ * Author : Rajesh kumar
+ * */
+
+@EnableEurekaServer
+@SpringBootApplication
+public class A1UniversalServiceRegistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A1UniversalServiceRegistoryApplication.class, args);
+	}
+
+}
