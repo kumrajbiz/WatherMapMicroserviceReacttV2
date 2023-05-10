9fa269142549e75580300f96e974f1631dd1e323
kumrajbiz <126977951+kumrajbiz@users.noreply.github.com>
2023-05-10 14:16:15 +0530

    React UI for Weather Map api
    Include  Docker file

---
+++ weather-app-reactV3/src/constants.js
@@ -0,0 +1,9 @@
+ // for Weathermap.com
+  //const apiKey = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=a38f758c5edcf066e4c15e4bd4284d5c`; 
+  // for localhost.com
+  //const apiKey = `http://localhost:8888/weather/${search}`;
+  // for external ip
+  //const apiKey = `http://172.18.0.4:8888/weather/${search}`;
+  // for docker container
+  const apiKey = `http://172.18.0.4:8888/weather/${search}`;
+  
